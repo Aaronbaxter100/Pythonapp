9dccfa6f143d7ee4d95b0a12e25fefb3c94b0db0
aaron <aaronbaxter200@gmailcom>
2024-09-24 15:28:01 +0100

    Label and start of frame

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ window = Tk()
 window.geometry('400x400')
 window.title('Testing')
 
+#Labels
+label1 = Label(text = ('Testingwelcome'), font = ('Arial', 30))
+label1.pack()
+
+#Frames
+left_frame = Frame(window, bg=blue)
+left_frame.grid()
+
 window.mainloop()
