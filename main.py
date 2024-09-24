db8fc3dc05840b6bd0979fc7f95dd65c3359402b
Aaronb1993 <aaronbaxter112@gmail.com>
2024-09-24 21:36:51 +0100

    Options frame for buttons

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from tkinter import*
 
 window = Tk()
-window.geometry('400x400')
+window.geometry('500x500')
 window.title('Testing')
 
+
+options_top = Frame(window, bg= 'blue')
+options_top.pack(pady=5)
+options_top.pack_propagate(False)
+options_top.configure(width=500, height= 55)
+
 #Labels
-label1 = Label(text = ('Testingwelcome'), font = ('Arial', 30))
-label1.pack()
+#label1 = Label(text = ('Testingwelcome'), font = ('Arial', 30))
+#label1.pack()
 
 #Frames
-left_frame = Frame(window, bg=blue)
-left_frame.grid()
+#left_frame = Frame(window, bg=blue)
+#left_frame.grid()
 
 window.mainloop()
