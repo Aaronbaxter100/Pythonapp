4abb6b56537e736d516abf1bb0b78c65a7de0e45
Aaronb1993 <aaronbaxter11@hotmail.com>
2024-09-30 20:06:28 +0100

    Tasks, TBC and Settings Page Function

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ def home_page():
 
     home_frame.pack(pady=20)
 
+def task_page():
+    task_frame = Frame(window)
+
+    lb = Label(task_frame,text='Task page\n\nPage: 2', font =('Bold', 30))
+    lb.pack()
+
+    task_frame.pack(pady=20)
+
+def TBC_page():
+    TBC_frame = Frame(window)
+
+    lb = Label(TBC_frame,text='Unsure page\n\nPage: 3', font =('Bold', 30))
+    lb.pack()
+
+    TBC_frame.pack(pady=20)
+
+def settings_page():
+    settings_frame = Frame(window)
+
+    lb = Label(settings_frame,text='settings page\n\nPage: 4', font =('Bold', 30))
+    lb.pack()
+
+    settings_frame.pack(pady=20)
+
 
 
 def hide_indicators():
@@ -44,7 +68,7 @@ Home_indicate.place(x=45, y=30, width=35, height=5)
 
 Tasks_button = Button(options_top, text='Tasks', font =('Arial',13),
                      bd=0, fg= '#0097e8',bg='light grey', activeforeground= 'grey',
-                    command=lambda: indicate(Tasks_indicate))
+                    command=lambda: indicate(Tasks_indicate,task_page()))
 
 Tasks_button.place(x=125, y=0, width=125)
 
@@ -53,7 +77,7 @@ Tasks_indicate.place(x=170, y=30, width=35, height=5)
 
 TBC_button = Button(options_top, text='Unsure', font =('Arial',13),
                     bd=0, fg= '#0097e8', bg='light grey', activeforeground= 'grey',
-                    command=lambda: indicate(TBC_indicate))
+                    command=lambda: indicate(TBC_indicate, TBC_page()))
 
 TBC_button.place(x=250, y=0, width=125)
 
@@ -62,7 +86,7 @@ TBC_indicate.place(x=297, y=30, width=35, height=5)
 
 Settings_button = Button(options_top, text='Settings', font =('Arial',13),
                     bd=0, fg= '#0097e8',bg='light grey', activeforeground= 'grey',
-                    command=lambda: indicate(Settings_indicate))
+                    command=lambda: indicate(Settings_indicate, settings_page()))
 
 Settings_button.place(x=375, y=0, width=125)
 
